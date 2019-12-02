c68ae221cffaff737b2301f4ceeaee47760d03ee
Lukasz Wrona <lukasz.andrzej.wrona@gmail.com>
2019-12-02 19:55:49 +0000

    Make tests use more functional syntax

--- AdventOfCodeTest/Day2Test.fs
+++ AdventOfCodeTest/Day2Test.fs
@@ -1,18 +1,19 @@
 ﻿namespace Test.Day2
 open Microsoft.VisualStudio.TestTools.UnitTesting
+open Test.Utils
 
 [<TestClass>]
 type All() = 
     [<TestMethod>]
     member this.TestAdd() =
-        Assert.AreEqual(Day2.func "1,0,0,0,99", "2,0,0,0,99") // 1 + 1 = 2
+        Day2.func "1,0,0,0,99" |> Assert.equal "2,0,0,0,99" // 1 + 1 = 2
     [<TestMethod>]
     member this.TestMultiplyOverwrite() =
-        Assert.AreEqual(Day2.func "2,3,0,3,99", "2,3,0,6,99") // 3 * 2 = 6.
+        Day2.func "2,3,0,3,99" |> Assert.equal "2,3,0,6,99" // 3 * 2 = 6.
     [<TestMethod>]
     member this.TestMultiply() =
-        Assert.AreEqual(Day2.func "2,4,4,5,99,0", "2,4,4,5,99,9801") // 99 * 99 = 9801.
+        Day2.func "2,4,4,5,99,0" |> Assert.equal "2,4,4,5,99,9801" // 99 * 99 = 9801.
     [<TestMethod>]
     member this.TestAll() =
-        Assert.AreEqual(Day2.func "1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99")
+        Day2.func "1,1,1,4,99,5,6,0,99" |> Assert.equal "30,1,1,4,2,5,6,0,99"
 
---
+++ AdventOfCodeTest/Utils.fs
@@ -0,0 +1,7 @@
+﻿module Test.Utils
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+type Assert with
+    static member equal (expected : 'u) (actual : 'u) =
+        Assert.AreEqual(expected, actual)
+
