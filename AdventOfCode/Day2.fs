ec8e0fbc3ae88c38ac5330d29581a470f1253c64
Lukasz Wrona <lukasz.andrzej.wrona@gmail.com>
2019-12-02 23:16:30 +0000

    More point-free stuff

--- AdventOfCode/Day2.fs
+++ AdventOfCode/Day2.fs
@@ -1,7 +1,7 @@
 ﻿module Day2
 open Extensions
 
-let deserialize (input : string) = input |> String.split ',' |> Seq.map int |> Seq.toList
+let deserialize = String.split ',' >> Seq.map int >> Seq.toList
 let serialize (input : int list) = input |> Seq.map string |> String.join ","
 
 let run (program : int list) : int list =
@@ -21,12 +21,10 @@ let run (program : int list) : int list =
 
     program |> step 0
 
-let runWithInputs a b input =
-    input
-    |> List.replaceAt 1 a
-    |> List.replaceAt 2 b
-    |> run
-    |> List.head
+let runWithInputs a b =
+    (function | head::_::_::rest -> head::a::b::rest)
+    >> run
+    >> List.head
 
 let findArgumentsFor (needle : int) (program : int list) : (int * int) seq =
     Seq.allPairs (Seq.init 99 id) (Seq.init 99 id)
