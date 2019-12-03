96b45e6e414b396d1efb6942d6535c0f2dc701e4
Lukasz A.J. Wrona <lukasz.andrzej.wrona@gmail.com>
2019-12-03 14:27:16 +0000

    Fix the thing

--- AdventOfCode/Day3.fs
+++ AdventOfCode/Day3.fs
@@ -49,32 +49,31 @@ let intersect segment1 segment2 =
         else None
     else None
 
-let rec countSteps pos dest = function
+// Count needs to be passed in for the tail call recursion to work - adding one
+// afterwards doesn't cut it
+let rec countSteps pos dest count = function
     | head :: tail ->
         let dirX, dirY, length = head
         let newPos = nextPos pos (dirX, dirY, 1)
         if newPos = dest then
-            Some 1
+            Some (count + 1)
         else if length > 1 then
-            (dirX, dirY, length - 1) :: tail
-            |> countSteps newPos dest
-            |> Option.map ((+) 1)
-        else
-            tail |> countSteps newPos dest |> Option.map ((+) 1)
+            (dirX, dirY, length - 1) :: tail |> countSteps newPos dest (count + 1)
+        else tail |> countSteps newPos dest (count + 1)
     | [] -> None
 
 let intersectionSteps (rawWires : string list) =
     let wires = rawWires |> List.map (parseWire >> Seq.toList)
     let segments = wires |> List.collect(wireToSegments (0, 0))
-    let closestIntersection =
-        Seq.allPairs segments segments
-        |> Seq.choose (fun (s1, s2) -> intersect s1 s2)
-        |> Seq.minBy(fun (x, y) -> abs(x) + abs(y))
-    wires
-    |> List.choose (countSteps (0, 0) closestIntersection)
-    |> List.sort
-    |> Seq.take 2
-    |> Seq.sum
+    Seq.allPairs segments segments
+    |> Seq.choose (fun (s1, s2) -> intersect s1 s2)
+    |> Seq.choose(fun intersection ->
+        wires
+        |> Seq.choose (countSteps (0, 0) intersection 0)
+        |> Seq.sort
+        |> Seq.tryTake 2
+        |> Option.map Seq.sum)
+    |> Seq.min
 
 let distance (wires : string list) =
     let segments = wires |> List.collect (parseWire >> Seq.toList >> wireToSegments (0, 0))
--- AdventOfCode/Extensions.fs
+++ AdventOfCode/Extensions.fs
@@ -8,6 +8,11 @@ module String =
 module Seq =
     let replaceAt index newValue list =
         list |> Seq.mapi(fun i value -> if i = index then newValue else value)
+    
+    let tryTake n elements =
+        // Horribly inefficient - consumes the entire sequence
+        if (Seq.length elements) < n then None
+        else elements |> Seq.take n |> Some
 
 module List =
     let replaceAt index newValue (list : list<'u>) =
