66c62b161ca6070999b58499fb3d28a3c387259c
Lukasz Wrona <lukasz.andrzej.wrona@gmail.com>
2019-12-08 18:25:42 +0000

    Rename "memory" to "state"

--- AdventOfCode/Day5.fs
+++ AdventOfCode/Day5.fs
@@ -7,7 +7,7 @@ let deserialize = String.split ',' >> Seq.map int >> Seq.toList
 let serialize (input : int list) = input |> Seq.map string |> String.join ","
 
 [<DebuggerDisplay("Program : {Index} {DebuggerOutput}")>]
-type Memory = {
+type State = {
     Index   : int
     Inputs  : int list
     Outputs : int list
@@ -51,7 +51,7 @@ let parseOpcode(opcode : int) : Instruction * (Mode list) =
         instruction, modes
         
 
-type Argument(state : Memory, mode : Mode, value : int) =
+type Argument(state : State, mode : Mode, value : int) =
     // Value of an argument ignoring value's mode
     member this.Raw = value
 
@@ -62,39 +62,39 @@ type Argument(state : Memory, mode : Mode, value : int) =
         | Immediate -> value
         | Positional -> program.[value]
 
-let runFull (inputs : int list) (program : int list) : Memory =
-    let rec step (memory : Memory) : Memory =
-        let program = memory.Program
-        let index = memory.Index
-        let inputs = memory.Inputs
-        let outputs = memory.Outputs
+let runFull (inputs : int list) (program : int list) : State =
+    let rec step (state : State) : State =
+        let program = state.Program
+        let index = state.Index
+        let inputs = state.Inputs
+        let outputs = state.Outputs
         let instruction, modes = parseOpcode program.[index]
         let values =
             Seq.zip (program |> Seq.skip (index + 1)) modes
-            |> Seq.map (fun (value, mode) -> Argument(memory, mode, value))
+            |> Seq.map (fun (value, mode) -> Argument(state, mode, value))
             |> Seq.toList
         let nextIndex = index + 1 + (Seq.length modes)
-        let apply func memory = match values with [a; b; output] ->
-            { memory with Program = program |> List.replaceAt (output.Raw) (func a.Value b.Value)
-                          Index = nextIndex }
-        let jumpIf func memory = match values with [condition; jumpTo] ->
-            if func condition.Value then { memory with Index = jumpTo.Value }
-            else { memory with Index = nextIndex }
+        let apply func state = match values with [a; b; output] ->
+            { state with Program = program |> List.replaceAt (output.Raw) (func a.Value b.Value)
+                         Index = nextIndex }
+        let jumpIf func state = match values with [condition; jumpTo] ->
+            if func condition.Value then { state with Index = jumpTo.Value }
+            else { state with Index = nextIndex }
         match instruction with
-        | Add -> memory |> apply (+) |> step
-        | Multiply -> memory |> apply (*) |> step
+        | Add -> state |> apply (+) |> step
+        | Multiply -> state |> apply (*) |> step
         | Read -> step {
-            memory with Program = program |> List.replaceAt (values.[0].Raw) (List.head inputs)
-                        Inputs = List.tail inputs
-                        Index = nextIndex }
+            state with Program = program |> List.replaceAt (values.[0].Raw) (List.head inputs)
+                       Inputs = List.tail inputs
+                       Index = nextIndex }
         | Write -> step {
-            memory with Outputs = outputs @ [ values.[0].Value ]
-                        Index = nextIndex }
-        | JumpIfTrue -> memory |> jumpIf ((<>) 0) |> step
-        | JumpIfFalse -> memory |> jumpIf ((=) 0) |> step
-        | LessThan -> memory |> apply (fun a b -> if a < b then 1 else 0) |> step
-        | Equals -> memory |> apply (fun a b -> if a = b then 1 else 0) |> step
-        | Stop -> memory
+            state with Outputs = outputs @ [ values.[0].Value ]
+                       Index = nextIndex }
+        | JumpIfTrue -> state |> jumpIf ((<>) 0) |> step
+        | JumpIfFalse -> state |> jumpIf ((=) 0) |> step
+        | LessThan -> state |> apply (fun a b -> if a < b then 1 else 0) |> step
+        | Equals -> state |> apply (fun a b -> if a = b then 1 else 0) |> step
+        | Stop -> state
     step { Index = 0; Inputs = inputs; Outputs = []; Program = program }
 
 let run (inputs : int list) (program : int list) = (runFull inputs program).Outputs
