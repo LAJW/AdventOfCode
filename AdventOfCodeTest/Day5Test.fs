863c68d781a3a75808da97fb4836e34d298f5b85
Lukasz Wrona <lukasz.andrzej.wrona@gmail.com>
2019-12-09 15:54:55 +0000

    Add day9 tests

--- AdventOfCodeTest/Day5Test.fs
+++ AdventOfCodeTest/Day5Test.fs
@@ -7,59 +7,59 @@ type Part2() =
     [<TestMethod>]
     member this.JumpIfElse() =
         let program = Day5.deserialize "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9"
-        program |> Day5.run [1] |> Assert.equal [1]
+        program |> Day5.run [1L] |> Assert.equal [1L]
 
     [<TestMethod>]
     member this.NotJumpIfElse() =
         let program = Day5.deserialize "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9"
-        program |> Day5.run [0] |> Assert.equal [0]
+        program |> Day5.run [0L] |> Assert.equal [0L]
 
     [<TestMethod>]
     member this.JumpIfElseImmediate() =
         let program = Day5.deserialize "3,3,1105,-1,9,1101,0,0,12,4,12,99,1"
-        program |> Day5.run [1] |> Assert.equal [1]
+        program |> Day5.run [1L] |> Assert.equal [1L]
 
     [<TestMethod>]
     member this.NotJumpIfElseImmediate() =
         let program = Day5.deserialize "3,3,1105,-1,9,1101,0,0,12,4,12,99,1"
-        program |> Day5.run [0] |> Assert.equal [0]
+        program |> Day5.run [0L] |> Assert.equal [0L]
 
     [<TestMethod>]
     member this.EqualTo8() =
         Day5.deserialize "3,9,8,9,10,9,4,9,99,-1,8"
-        |> Day5.run [8] |> Assert.equal [1]
+        |> Day5.run [8L] |> Assert.equal [1L]
         
     [<TestMethod>]
     member this.NotEqualTo8() =
         Day5.deserialize "3,9,8,9,10,9,4,9,99,-1,8"
-        |> Day5.run [1] |> Assert.equal [0]
+        |> Day5.run [1L] |> Assert.equal [0L]
 
     [<TestMethod>]
     member this.LessThan8() =
         Day5.deserialize "3,9,7,9,10,9,4,9,99,-1,8"
-        |> Day5.run [1] |> Assert.equal [1]
+        |> Day5.run [1L] |> Assert.equal [1L]
 
     [<TestMethod>]
     member this.NotLessThan8() =
         Day5.deserialize "3,9,7,9,10,9,4,9,99,-1,8"
-        |> Day5.run [8] |> Assert.equal [0]
+        |> Day5.run [8L] |> Assert.equal [0L]
 
     [<TestMethod>]
     member this.EqualTo8Immediate() =
         Day5.deserialize "3,3,1108,-1,8,3,4,3,99"
-        |> Day5.run [8] |> Assert.equal [1]
+        |> Day5.run [8L] |> Assert.equal [1L]
 
     [<TestMethod>]
     member this.NotEqualTo8Immediate() =
         Day5.deserialize "3,3,1108,-1,8,3,4,3,99"
-        |> Day5.run [1] |> Assert.equal [0]
+        |> Day5.run [1L] |> Assert.equal [0L]
 
     [<TestMethod>]
     member this.LessThan8Immediate() =
         Day5.deserialize "3,3,1107,-1,8,3,4,3,99"
-        |> Day5.run [7] |> Assert.equal [1]
+        |> Day5.run [7L] |> Assert.equal [1L]
 
     [<TestMethod>]
     member this.NotLessThan8Immediate() =
         Day5.deserialize "3,3,1107,-1,8,3,4,3,99"
-        |> Day5.run [8] |> Assert.equal [0]
+        |> Day5.run [8L] |> Assert.equal [0L]
---
+++ AdventOfCodeTest/Day9Test.fs
@@ -0,0 +1,31 @@
+﻿namespace Test.Day9
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Test.Utils
+
+[<TestClass>]
+type All() =
+
+    [<TestMethod>]
+    member this.OutputCopyOfItself() =
+        "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
+        |> Day5.deserialize
+        |> Day5.run []
+        |> Day5.serialize
+        |> Assert.equal "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
+        
+    [<TestMethod>]
+    member this.Output16DigitNumber() =
+        "1102,34915192,34915192,7,4,7,99,0"
+        |> Day5.deserialize
+        |> Day5.run []
+        |> Day5.serialize
+        |> String.length
+        |> Assert.equal 16
+
+    [<TestMethod>]
+    member this.OutputTheLargeNumberInTheMiddle() =
+        "104,1125899906842624,99"
+        |> Day5.deserialize
+        |> Day5.run []
+        |> Assert.equal [1125899906842624L]
+
