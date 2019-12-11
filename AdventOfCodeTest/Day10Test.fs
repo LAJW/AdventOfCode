4047f443a7533c7264a321b3a701d5a3e7d84803
Lukasz Wrona <lukasz.andrzej.wrona@gmail.com>
2019-12-11 12:10:26 +0000

    Add tests for Day10 part 1

---
+++ AdventOfCodeTest/Day10Test.fs
@@ -0,0 +1,60 @@
+﻿namespace Test.Day10
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Test.Utils
+
+[<TestClass>]
+type FindBestStation() =
+    [<TestMethod>]
+    member this.Map1() =
+        let map = ".#..#\
+                   .....\
+                   #####\
+                   ....#\
+                   ...##"
+        let parsed = Day10.parse 5 5 map
+        parsed |> Day10.findBestStationLocation |> Assert.equal (3, 4)
+
+    [<TestMethod>]
+    member this.Map2() =
+        let map = "......#.#.\
+                   #..#.#....\
+                   ..#######.\
+                   .#.#.###..\
+                   .#..#.....\
+                   ..#....#.#\
+                   #..#....#.\
+                   .##.#..###\
+                   ##...#..#.\
+                   .#....####"
+        let parsed = Day10.parse 10 10 map
+        parsed |> Day10.findBestStationLocation |> Assert.equal (5, 8)
+
+    [<TestMethod>]
+    member this.Map3() =
+        let map = "#.#...#.#.\
+                   .###....#.\
+                   .#....#...\
+                   ##.#.#.#.#\
+                   ....#.#.#.\
+                   .##..###.#\
+                   ..#...##..\
+                   ..##....##\
+                   ......#...\
+                   .####.###."
+        let parsed = Day10.parse 10 10 map
+        parsed |> Day10.findBestStationLocation |> Assert.equal (1, 2)
+
+    [<TestMethod>]
+    member this.Map4() =
+        let map = ".#..#..###\
+                   ####.###.#\
+                   ....###.#.\
+                   ..###.##.#\
+                   ##.##.#.#.\
+                   ....###..#\
+                   ..#.#..#.#\
+                   #..#.#.###\
+                   .##...##.#\
+                   .....#.#.."
+        let parsed = Day10.parse 10 10 map
+        parsed |> Day10.findBestStationLocation |> Assert.equal (6, 3)
