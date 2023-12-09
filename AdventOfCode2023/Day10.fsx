07159f1a082423c3656d7a4bf871951f358a993d
Łukasz Wrona <lukasz.andrzej.wrona@outlook.com>
2023-12-09 10:26:55 +0000

    Implement Day 9

---
+++ AdventOfCode2023/Day10.fsx
@@ -0,0 +1,15 @@
+﻿#r "./bin/Debug/net7.0/FSharpPlus.dll"
+
+open System
+
+open FSharpPlus
+open System.IO
+
+let inline value(self : ^T when ^T : (member Value : ^V)) = (^T : (member Value : ^V) self)
+
+let join (delim : string) (strs : string seq) = String.Join(delim, strs)
+
+let data = """
+""".Split("\n")
+
+let data = File.ReadAllLines "./AdventOfCode2023/data9.txt"
---
+++ AdventOfCode2023/Day9.fs
@@ -0,0 +1,19 @@
+﻿open System
+open FSharpPlus
+open System.IO
+
+let data = File.ReadAllLines "./data9.txt"
+
+let rows = data |> Seq.map (String.split [" "] >> map Int32.Parse >> toList) |> toList
+
+let proc = Seq.pairwise >> map (fun (a, b) -> b - a) >> toList
+
+let solve postProc =
+    rows |> Seq.map (fun row ->
+        (row, (Seq.init row.Length id)) ||> Seq.scan (fun state _ -> proc state)
+        |> takeWhile (exists ((<>) 0))
+        |> postProc
+    ) |> sum
+
+solve (map Seq.last >> sum) |> printfn "Part 1: %d"
+solve (map head >> Seq.reduceBack (-)) |> printfn "Part 2: %d"
