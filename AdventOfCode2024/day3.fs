f8f7ecff04b0e2504c1140e91d0c8b92c795a923
Łukasz Wrona <lukasz.andrzej.wrona@outlook.com>
2024-12-03 05:39:19 +0000

    Solution day 3

--- AdventOfCode2024/day3.fs
+++ AdventOfCode2024/day3.fs
@@ -1,8 +1,30 @@
 module AdventOfCode2024.day3
 
-let run1() =
-    let lines = File.ReadAllLines "./data3.txt"
+open System.Text.RegularExpressions
+open System.IO
 
-    let numbers = lines |> map(String.split [" "] >> Seq.map int >> toList)
-    
-    printfn "%d" result
+let run1 () =
+    let text = File.ReadAllText "./data3.txt"
+
+    Regex.Matches(text, @"mul\((\d+),(\d+)\)")
+    |> Seq.sumBy (fun m ->
+        match m.Groups |> Seq.toList with
+        | [ _; a; b ] -> int a.Value * int b.Value
+        | _ -> failwith "unreachable"
+    )
+    |> printfn "%d"
+
+let run2 () =
+    let text = File.ReadAllText "./data3.txt"
+
+    Regex.Matches(text, @"mul\((\d+),(\d+)\)|do\(\)|don't\(\)")
+    |> Seq.map (fun m -> m.Groups |> Seq.map (_.Value) |> Seq.toList)
+    |> Seq.fold (fun (sum, enabled) matches ->
+        match matches with
+        | "do()" :: _ -> (sum, true)
+        | "don't()" :: _ -> (sum, false)
+        | [_; a; b] when enabled -> (sum + int a * int b, enabled)
+        | _ -> (sum, enabled)
+    ) (0, true)
+    |> fst
+    |> printfn "%d"
