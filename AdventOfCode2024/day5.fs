8c91c1a4b720251f10b9e3c6ce11df29fd1c5f8a
Łukasz Wrona <lukasz.andrzej.wrona@outlook.com>
2024-12-05 05:47:41 +0000

    Refactor common parts

--- AdventOfCode2024/day5.fs
+++ AdventOfCode2024/day5.fs
@@ -3,77 +3,54 @@ module AdventOfCode2024.day5
 open FSharpPlus
 open System.IO
 
-let run1 () =
+let orderingAndUpdates() = 
     let lines = File.ReadAllText("data5.txt")
     let [| top; bottom |] = lines.Split("\n\n")
 
     let ordering =
         top.Split("\n")
-        |> Seq.map (String.split [ "|" ] >> Seq.map int >> toList)
-        |> Seq.groupBy Seq.head
+        |> Seq.map (String.split [ "|" ] >> Seq.map int)
+        |> Seq.groupBy head
         |> Map
-        |> Map.mapValues (Seq.map Seq.last >> Seq.toList)
+        |> Map.mapValues (Seq.map Seq.last >> toList)
 
     let updates =
         bottom.Split("\n")
         |> Seq.filter ((<>) "")
         |> Seq.map (String.split [ "," ] >> Seq.map int >> toList)
         |> toList
+        
+    ordering, updates
 
+let inRightOrder (ordering : Map<int, int list>) (update: int list) =
+    seq { 0 .. update.Length - 1 }
+    |> Seq.forall (fun i ->
+        let first = update[i]
+        let rest = update[i + 1 ..]
+        let order = ordering.TryFind(first) |> Option.defaultValue []
+        Set.isSubset (Set rest) (Set order))
+    
+let middleNumber (update: int list) = update[update.Length / 2]
 
-    let inRightOrder (update: int list) =
-        seq { 0 .. update.Length - 1 }
-        |> Seq.forall (fun i ->
-            let first = update[i]
-            let rest = update[i + 1 ..]
-            let order = ordering.TryFind(first) |> Option.defaultValue []
-            Set.isSubset (Set rest) (Set order))
+let run1 () =
+    let ordering, updates = orderingAndUpdates()
+    let inRightOrder = inRightOrder ordering
 
-    let middleNumber (update: int list) = update[update.Length / 2]
-
-    let result = updates |> Seq.filter inRightOrder |> Seq.map middleNumber |> Seq.sum
-
-    printfn "%d" result
+    updates |> Seq.filter inRightOrder |> Seq.map middleNumber |> Seq.sum |> printfn "%d"
 
 let run2 () =
-    let lines = File.ReadAllText("data5.txt")
-    let [| top; bottom |] = lines.Split("\n\n")
+    let ordering, updates = orderingAndUpdates()
+    let inRightOrder = inRightOrder ordering
 
-    let ordering =
-        top.Split("\n")
-        |> Seq.map (String.split [ "|" ] >> Seq.map int >> toList)
-        |> Seq.groupBy Seq.head
-        |> Map
-        |> Map.mapValues (Seq.map Seq.last >> Seq.toList)
-
-    let updates =
-        bottom.Split("\n")
-        |> Seq.filter ((<>) "")
-        |> Seq.map (String.split [ "," ] >> Seq.map int >> toList)
-        |> toList
-
-
-    let inRightOrder (update: int list) =
-        seq { 0 .. update.Length - 1 }
-        |> Seq.forall (fun i ->
-            let first = update[i]
-            let rest = update[i + 1 ..]
-            let order = ordering.TryFind(first) |> Option.defaultValue []
-            Set.isSubset (Set rest) (Set order))
-
-    let middleNumber (update: int list) = update[update.Length / 2]
-
-    let result =
-        updates
-        |> Seq.filter (not << inRightOrder)
-        |> Seq.map (fun x ->
-            x
-            |> List.sortWith (fun a b ->
-                if ordering.TryFind(a) |> Option.defaultValue [] |> List.contains b then
-                    -1
-                else
-                    1))
-        |> Seq.map middleNumber
-        |> Seq.sum
-
-    printfn "%d" result
+    updates
+    |> Seq.filter (not << inRightOrder)
+    |> Seq.map (fun x ->
+        x
+        |> List.sortWith (fun a b ->
+            if ordering.TryFind(a) |> Option.defaultValue [] |> List.contains b then
+                -1
+            else
+                1))
+    |> Seq.map middleNumber
+    |> Seq.sum
+    |> printfn "%d"
