95fbc93f07c2b39873ea3d441dd202726f2b9a03
Łukasz Wrona <lukasz.andrzej.wrona@outlook.com>
2024-12-07 06:30:18 +0000

    Day 8 prep

---
+++ AdventOfCode2024/day8.fs
@@ -0,0 +1,13 @@
+module AdventOfCode2024.day8
+
+open System
+open System.IO
+open FSharpPlus
+
+let run1() =
+    let lines = File.ReadAllLines("data8.txt")
+    ()
+
+let run2() =
+    let lines = File.ReadAllLines("data8.txt")
+    ()
