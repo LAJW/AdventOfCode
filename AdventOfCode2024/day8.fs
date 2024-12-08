8d940586f8b61d4c12ce07ecb58f63fd776ca70b
Łukasz Wrona <lukasz.andrzej.wrona@outlook.com>
2024-12-08 10:38:04 +0000

    Refactor day8 part 1 & 2

--- AdventOfCode2024/day8.fs
+++ AdventOfCode2024/day8.fs
@@ -5,7 +5,7 @@ open System.IO
 open FSharpPlus
 open AdventOfCode2024.Utils
 
-let run1 () =
+let solve (getAntinodes: Vec -> Vec -> char Grid -> Vec seq) =
     let grid = File.ReadAllLines("data8.txt") |> Grid.fromLines
 
     let map =
@@ -25,47 +25,27 @@ let run1 () =
         |> Map.values
         |> Seq.collect (fun positions -> positions |> Seq.allPairs positions)
         |> Seq.filter (fun (a, b) -> a <> b)
-        |> Seq.collect (fun (a, b) ->
-            let diff = b - a
-            [ (a - diff); (b + diff) ])
+        |> Seq.collect (fun (a, b) -> getAntinodes a b grid)
         |> Seq.filter (fun pos -> grid |> Grid.hasIndex pos)
         |> Set
 
     antinodes.Count |> printfn "%d"
+
+let run1 () =
+    solve (fun a b _ ->
+        let diff = b - a
+        [ (a - diff); (b + diff) ])
 
 let run2 () =
-    let grid = File.ReadAllLines("data8.txt") |> Grid.fromLines
+    solve (fun a b grid ->
+        let diff = b - a
 
-    let map =
-        grid
-        |> Grid.enumerate
-        |> Seq.choose (fun (pos, node) ->
-            if Char.IsNumber(node) || Char.IsLetter(node) then
-                Some(pos, node)
-            else
-                None)
-        |> Seq.groupBy snd
-        |> Seq.map (fun (key, nodes) -> key, (nodes |> Seq.map fst |> toList))
-        |> Map
+        let before =
+            Seq.initInfinite (fun i -> a - diff * i)
+            |> Seq.takeWhile (fun pos -> grid |> Grid.hasIndex pos)
 
-    let antinodes =
-        map
-        |> Map.values
-        |> Seq.collect (fun positions -> positions |> Seq.allPairs positions)
-        |> Seq.filter (fun (a, b) -> a <> b)
-        |> Seq.collect (fun (a, b) ->
-            let diff = b - a
+        let after =
+            Seq.initInfinite (fun i -> b + diff * i)
+            |> Seq.takeWhile (fun pos -> grid |> Grid.hasIndex pos)
 
-            let before =
-                Seq.initInfinite (fun i -> a - diff * i)
-                |> Seq.takeWhile (fun pos -> grid |> Grid.hasIndex pos)
-
-            let after =
-                Seq.initInfinite (fun i -> b + diff * i)
-                |> Seq.takeWhile (fun pos -> grid |> Grid.hasIndex pos)
-
-            Seq.append before after)
-        |> Seq.filter (fun pos -> grid |> Grid.hasIndex pos)
-        |> Set
-
-    antinodes.Count |> printfn "%d"
+        Seq.append before after)
